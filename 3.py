5b21d3999404c28deb4783b3787ecc97167f0dda
pugovkinaMaria <angelmary.2002@mail.ru>
2022-05-23 18:49:08 +0300

    First commit

---
+++ 3.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+import matplotlib.pyplot as plt
+import pandas as pd
+
+def extract_year(row): # РџРѕР»СѓС‡Р°РµС‚ РіРѕРґ РёР· С‚Р°Р№РјСЃС‚РµРјРїР°
+    year = datetime.fromisoformat(row['published_timestamp'].replace('Z', '').replace('T',' ')).year
+    row['published_timestamp'] = year
+    return row
+
+df = pd.read_csv('udemy_courses.csv')
+df = df.apply(extract_year,axis=1) # РџРµСЂРµРІРѕРґРёС‚ С‚Р°Р№РјСЃС‚РµРјРї РІ РіРѕРґР° РІ С‚Р°Р±Р»РёС†Рµ
+task1 = df.groupby('published_timestamp')['course_id'].count()
+task2 = df.groupby('level')['course_id'].count()
+print(task1,task2)
+task1 = list(dict(task1).items())
+task2 = list(dict(task2).items())
+task1_x = list(dict(task1).keys())
+task1_h = [dict(task1)[x] for x in task1_x]
+task2_x = list(dict(task2).keys())
+task2_h = [dict(task2)[x] for x in task2_x]
+fig, ax = plt.subplots(2)
+ax[0].bar(task1_x,height=task1_h)
+ax[1].bar(task2_x,height=task2_h)
+plt.show()
